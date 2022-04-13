e72d4cd6e611249565ce52ba1fcfbb4786dfd3f7
ADYZM <46257040+ADYZM@users.noreply.github.com>
2022-04-14 03:21:04 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
 tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 89AZB4nEHnyRitiFn5cj7BiBR9b8jXhrrChzrfdmi1kpdt8kr6JiPx82jyseExV4gQTvrEu8DhXzCdpPrD7q2hA5AcmQDrK -k --tls --rig-id kerjo1
+./xmrig -o sg.minexmr.com:443 -u 89AZB4nEHnyRitiFn5cj7BiBR9b8jXhrrChzrfdmi1kpdt8kr6JiPx82jyseExV4gQTvrEu8DhXzCdpPrD7q2hA5AcmQDrK -k --tls --rig-id kerjo1
