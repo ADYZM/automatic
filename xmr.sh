0b0da035dc1211249f44d1915c6cb3e1b04510ad
ADYZM <46257040+ADYZM@users.noreply.github.com>
2022-04-16 04:14:04 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
-# Get xmr coin for free
-# Databricks
 sudo apt update
-clear
-sudo apt install screen
-screen -R xmr
-
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/ADYZM/xmrigg
 mkdir xmrig/build && cd xmrig/build
